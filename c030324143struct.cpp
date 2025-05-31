55ad84a5435811cec9db9b967b280381e05249f8
muhammad-rizqi8 <ffgeming71@gmail.com>
2025-05-31 20:59:07 +0800

    pointer berhasil

--- c030324143struct.cpp
+++ c030324143struct.cpp
@@ -1,23 +1,27 @@
-#include <conio.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <string>
 using namespace std;
 
- struct mahasiswa{
-	char nim[11];
-	char nama[30];
-	float ipk;
+struct Mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    float ipk;
 };
-	int main(){
-		mahasiswa mhs;
-		
- 		printf("NIM = c030324143 "); 		scanf("%s",&mhs.nim);  fflush(stdin);	 		
-		printf("Nama = Muhammad Rizqi ");		scanf("%s",&mhs.nama); fflush(stdin);
-		printf("IPK = 3,23 ");		scanf("%f",&mhs.ipk);
-		
- 		printf("\nData Anda :\n");
-		printf("NIM  = %s\n",mhs.nim);
- 		printf("Nama = %s\n",mhs.nama);
- 		printf("IPK  = %.2f\n",mhs.ipk);
-		
-	}
+
+int main() {
+    Mahasiswa rizqi;
+    Mahasiswa* ptr = &rizqi; // pointer ke struct
+
+    ptr->nim = "c030324143";
+    ptr->nama = "Muhammad Rizqi";
+    ptr->alamat = "Jl. Singgosari";
+    ptr->ipk = 3.23;
+
+    cout << "NIM    : " << ptr->nim << "\n";
+    cout << "Nama   : " << ptr->nama << "\n";
+    cout << "Alamat : " << ptr->alamat << "\n";
+    cout << "IPK    : " << ptr->ipk << "\n";
+
+	return 0;
+}
