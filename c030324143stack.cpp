6782b22936e13b2d0a2fb44851b3b0dffde01a90
muhammad-rizqi8 <ffgeming71@gmail.com>
2025-06-03 16:16:00 +0800

    menambah kan fungsi main dan push test dan pop test

--- c030324143stack.cpp
+++ c030324143stack.cpp
@@ -63,8 +63,33 @@ bool pop() {
     delete temp;
     count--;
 
-    cout << "Pop" << value << " sukses. (total: " << count << ")"
+    cout << "Pop" << value << " sukses. (total: " << count << ")" << endl;
+    return true;
     
 }
 
+};
+
+int main () {
+    stack s;
+
+    cout << "--- PUSH TEST ---" << endl;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.push(50);
+    s.push(60); // Harus gagal
+
+    cout << "\n--- POP TEST ---" << endl;
+    s.pop();
+    s.pop();
+    s.pop();
+    s.pop();
+    s.pop();
+    s.pop(); // Harus gagal
+
+    return 0;
+    
+    
 }
